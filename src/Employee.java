b79cd6debc8da9e2fab6b14f80e9c32edcb381b8
shajn786 <shajn786@gmail.com>
2022-11-21 15:41:36 +0530

    add employee

---
+++ src/Employee.java
@@ -0,0 +1,48 @@
+public class Employee {
+
+    int empcode,salary,phonenumber;
+    String empname,designation,companyname,address,email;
+
+    public Employee(int empcode,String empname,String designation,int salary, int phonenumber,String email,String address)
+    {
+        this.empcode =empcode;
+        this.empname = empname;
+        this.designation= designation;
+        this.salary =salary;
+        this.phonenumber = phonenumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getEmpcode() {
+        return empcode;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public String getCompanyname() {
+        return companyname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
